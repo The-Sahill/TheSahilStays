2b9af8cceb185bbc4fed3b311e2f9994cc6ced37
MRERROR2050 <abdalfatah.aljuaidi@gmail.com>
2026-09-12 18:15:36 +0300

    d

--- frontend/src/components/transportation/UpdateRequest.jsx
+++ frontend/src/components/transportation/UpdateRequest.jsx
@@ -412,9 +412,9 @@ console.log(error)
                                             onChange={handleChange} 
                                             className="w-full px-4 py-3 rounded-xl border border-gray-200 text-sm bg-white focus:outline-none focus:border-[#1b2a32]"
                                         >
-                                            <option value="بانتظار الموافقة">بانتظار الموافقة</option>
-                                            <option value="تمت الموافقة">تمت الموافقة</option>
-                                            <option value="مكتمل">مكتمل</option>
+                                            <option value="بانتظار الموافقة">بانتظار الموافقة على النقل</option>
+                                            <option value="تمت الموافقة">تمت الموافقة على النقل</option>
+                                            <option value="مكتمل">تم الصرف (تم الدفع)</option>
                                         </select>
                                     </div>
 
